cbde1771b464474fbfe4a3b9f88e74399d166bb1
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-05-05 19:05:32 +0200

    zad

--- Main.java
+++ Main.java
@@ -1,20 +1,70 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import company.abstracts.Employee;
 import company.models.Manager;
 import company.models.Worker;
+
 
 public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("David", 8000, 1, "2022-01-01", "Junior Developer");
-        Worker worker2 = new Worker("Mark", 3800, 1, "2023-03-15", "Senior Developer");
-        Worker worker3 = new Worker("Bob", 3400, 3, "2020-12-10", "Tester");
-        
-        Manager manager = new Manager("Michael", 5000, 5, "2019-05-10", "Team Head");
+        Worker worker2 = new Worker("Mark", 3800, 2, "2023-03-15", "Senior Developer");
+        Worker worker3 = new Worker("Bob", 3400, 2, "2020-12-10", "Tester");
+        Worker worker4 = new Worker("Rob", 5400, 4, "2020-12-10", "Tester");
+        Worker worker5 = new Worker("Harry", 4500, 4, "2020-12-10", "Tester");
 
-        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
-        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
-        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
-        System.out.println(manager.getName() + " has code: " + manager.hashCode());
-        System.out.println(worker1.getName() + " equals worker3: " + worker3.equals(worker1));
-        System.out.println(worker1.getName() + " equals worker2: " + worker3.equals(worker2));
-        System.out.println(worker1.getName() + " equals manager: " + worker3.equals(manager));
+        Manager manager1 = new Manager("Michael", 5000, 5, "2019-05-10", "Team Head");
+        Manager manager2 = new Manager("Michael", 5000, 5, "2019-05-10", "Team Lead");
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(worker5);
+        employees.add(manager1);
+        employees.add(manager2);
+
+        double totalSalary = 0;
+        double totalManagerSalary = 0;
+        double totalWorkerSalary = 0;
+
+        for (Employee emp : employees) {
+            totalSalary += emp.getSalary();
+            if (emp instanceof Manager) {
+                totalManagerSalary += emp.getSalary();
+            } else if (emp instanceof Worker) {
+                totalWorkerSalary += emp.getSalary();
+            }
+        }
+
+        System.out.println("Całkowita suma pensji wszystkich pracowników: " + totalSalary);
+        System.out.println("Całkowita suma pensji wszystkich menedżerów: " + totalManagerSalary);
+        System.out.println("Całkowita suma pensji wszystkich pracowników: " + totalWorkerSalary);
+
+        Map<Integer, List<Employee>> idMap = new HashMap<>();
+        for (Employee emp : employees) {
+            int id = emp.hashCode();
+            if (!idMap.containsKey(id)) {
+                idMap.put(id, new ArrayList<>());
+            }
+            idMap.get(id).add(emp);
+        }
+
+        for (Map.Entry<Integer, List<Employee>> entry : idMap.entrySet()) {
+            int id = entry.getKey();
+            List<Employee> employeesWithSameId = entry.getValue();
+            if (employeesWithSameId.size() > 1) {
+                System.out.println("Obiekty o identyfikatorze " + id + ":");
+                for (Employee emp : employeesWithSameId) {
+                    System.out.println("- " + emp.getName() + ", stanowisko: " + emp.getPosition());
+                }
+            }
+        }
     }
 }
+
+
